c60174d8c478847a0b9ebaa7dbee0f81d7053b7a
Rohan <rohan.sivam2562@gmail.com>
2024-09-18 11:10:45 +1000

    created the initial structure for the project and added an article card reusable component

---
+++ components/article.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { TouchableOpacity, Image, View, Text, Linking, StyleSheet } from 'react-native';
+
+interface ArticleCardProps {
+  item: {
+    url: string;
+    urlToImage: string;
+    title: string;
+    description: string;
+  };
+}
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ item }) => {
+  return (
+    <TouchableOpacity style={styles.articleContainer} onPress={() => Linking.openURL(item.url)}>
+      <Image source={{ uri: item.urlToImage }} style={styles.articleImage} />
+      <View style={styles.articleDetails}>
+        <Text style={styles.articleTitle}>{item.title}</Text>
+        <Text style={styles.articleDescription}>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  articleContainer: {
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    overflow: 'hidden',
+    elevation: 2, // For shadow in Android
+  },
+  articleImage: {
+    width: '100%',
+    height: 200,
+  },
+  articleDetails: {
+    padding: 10,
+  },
+  articleTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  articleDescription: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
+
+export default ArticleCard;
